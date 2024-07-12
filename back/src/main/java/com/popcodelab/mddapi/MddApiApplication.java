a29db08e2bb70197b23f30875e2b3515daba510f
POP's Code Lab <pop.code.lab13@gmail.com>
2024-07-12 19:29:44 +0700

    POM & App configuration file added

--- back/src/main/java/com/popcodelab/mddapi/MddApiApplication.java
+++ back/src/main/java/com/popcodelab/mddapi/MddApiApplication.java
@@ -1,12 +1,15 @@
-package com.openclassrooms.mddapi;
+package com.popcodelab.mddapi;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Log4j2
 public class MddApiApplication {
 
 	public static void main(String[] args) {
+		log.debug("Starting MDD - Minimum Viable Product version");
 		SpringApplication.run(MddApiApplication.class, args);
 	}
 
--- back/src/test/java/com/popcodelab/mddapi/MddApiApplicationTests.java
+++ back/src/test/java/com/popcodelab/mddapi/MddApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.openclassrooms.mddapi;
+package com.popcodelab.mddapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
